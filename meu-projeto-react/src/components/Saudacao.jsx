91eaca6722f7ff7ae223a3bf2dd3c5634b45b2f2
YoranHansel <yoranhansel@gmail.com>
2026-06-21 13:00:13 -0300

    Componente de saudação
    adicionei o componente de Saudacao para saudar quem visitar o site.

---
+++ meu-projeto-react/src/components/Saudacao.jsx
@@ -0,0 +1,10 @@
+function Saudacao(){
+    return(
+        <div>
+            <h1>Olá, seja bem-vindo ao meu projeto React!</h1>
+            <p>Este é um componente de saudação simples.</p>
+        </div>
+    );
+}
+
+export default Saudacao;
